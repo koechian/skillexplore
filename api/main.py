4e0f334b913285b985f0adbd688823cdce852cda
--global <--global>
2024-01-01 20:46:08 +0300

    API Init

--- api/main.py
+++ api/main.py
@@ -1,12 +1,33 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+import os
+
+UPLOAD_FOLDER = "uploads"
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-@app.route("/")
-def home():
-    return "Hello World"
+def cleaner():
+    files = os.listdir("uploads")
+    for file in files:
+        os.remove("uploads/" + file)
+
+
+@app.route("/resume", methods=["POST"])
+def uploadedResume():
+    if "file" not in request.args:
+        return "No file found"
+    file = request.files["file"]
+
+    if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+        os.makedirs(app.config["UPLOAD_FOLDER"])
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
+
+        return "File uploaded successfully!"
+
+    return "default, shits wrong"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # cleaner()
+    app.run()
